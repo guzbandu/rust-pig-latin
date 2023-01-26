9ff4a6fc85b43a5021c39e10f112a4f1527072b6
guzbandu <jjen.franklin@gmail.com>
2023-01-26 09:19:35 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use std::io;
+
+fn main() {
+    let mut sentence = String::from("start");
+    let vowels: Vec<char> = vec!['a', 'e', 'i', 'o', 'u', 'y'];
+    while sentence != "q".to_string() {	
+        sentence = "".to_string();
+        println!("Enter a sentence and it will be converted to Pig Latin!");
+        println!("Type q to quit!");
+        
+        io::stdin()
+             .read_line(&mut sentence)
+             .expect("Failed to read line");
+
+        sentence = sentence.trim().to_string();
+        
+        let mut pig_latin = String::new();
+        if sentence != "q" {
+            let mut first_word = true;
+            for word in sentence.split(" ") {
+                if first_word == true {
+                    first_word = false;
+                } else {
+                    pig_latin.push_str(" ");
+                }
+                let char_array: Vec<char> = word.chars().collect();
+                let first_char = char_array[0];
+                //println!("{word} {first_char}");
+                let mut consonent = true;
+                for vwl in &vowels {
+                    //println!("{vwl}");
+                    if *vwl == first_char {
+                        consonent = false;
+                        //println!("{first_char} is a vowel!!");
+                    }
+                }
+                //println!("{first_char} is a consonent {consonent}");
+                if consonent == true {
+                    pig_latin.push_str(&word[1..]);
+                    pig_latin.push_str("-");
+                    pig_latin.push_str(&first_char.to_string());
+                    pig_latin.push_str("ay");
+                } else {
+                    pig_latin.push_str(word);
+                    pig_latin.push_str("-");
+                    pig_latin.push_str("hay");
+                }
+            }
+        }
+
+        //println!("You entered:");
+        //println!("{:?}",sentence);
+        if sentence != "q" {
+            println!("In Pig Latin that is:");
+            println!("{:?}",pig_latin);
+        }
+    }
+}
